c3cc96ff2302eb8ec8aaec1d304c0bc9ba4e0e15
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2024-05-27 09:36:28 +0530

    First Commit

--- employee.py
+++ employee.py
@@ -15,7 +15,6 @@ from langchain.memory import ConversationSummaryMemory
 from langchain.chains import ConversationalRetrievalChain
 # from tempfile import NamedTemporaryFile
 from fpdf import FPDF
-from io import BytesIO
 
 __import__('pysqlite3')
 import sys
@@ -110,24 +109,25 @@ def generate_evaluation_report(emp_id):
     """
     return report
 
-        # Create HTML content from the response
-    html_content = f/"""
-    <html>
-    <head>
-    <meta charset="utf-8">
-    <style>
-        body {{
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }}
-    </style>
-    </head>
-    <body>
-        {report.replace('\n', '<br>')}
-    </body>
-    </html>
-    """
-    return html_content
+def download_pdf():
+    # Response variable to write in the PDF
+    response_variable = "This is the content that will be written to the PDF."
+
+    # Create instance of FPDF class
+    pdf = FPDF()
+
+    # Add a page
+    pdf.add_page()
+
+    # Set font
+    pdf.set_font("Arial", size=12)
+
+    # Add a cell
+    pdf.cell(200, 10, txt=response_variable, ln=True, align='C')
+
+    # Save the PDF with name
+    pdf.output("response_variable.pdf")
+    
 
 st.title("Employee Evaluation Report")
 
@@ -144,27 +144,18 @@ if submit_button:
         st.session_state.html_content = generate_evaluation_report(emp_id)
 
     st.write("### Employee Evaluation Report")
+    download_pdf()
     st.markdown(st.session_state.html_content, unsafe_allow_html=True)
     
+# Path to your file
+file_path = 'response_variable.pdf'
 
-if download_btn:
-    # Create instance of FPDF class
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
-    response_variable = "This is the content that will be written to the PDF."
-    pdf.cell(200, 10, txt=response_variable, ln=True, align='C')
-
-    # Save to a BytesIO object
-    pdf_output = BytesIO()
-    pdf.output(pdf_output, 'F')
-
-    # Reset the buffer's cursor position
-    pdf_output.seek(0)
-
-    # Provide download button
-    st.download_button(label="Download your PDF",
-                    data=pdf_output.getvalue(),
-                    file_name="response_variable.pdf",
-                    mime='application/pdf')
-
+# Open the file in binary mode
+with open(file_path, "rb") as file:
+    btn = st.download_button(
+            label="Download PDF",
+            data=file,
+            file_name="data.pdf",
+            mime="application/pdf"
+        )
+    
